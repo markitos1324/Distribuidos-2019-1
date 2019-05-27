035e1003cf326ce8254eab6242ee53a2d1492e25
Marco Antonio Gonzalez Martinez <markiros1324@gmail.com>
2019-05-27 18:43:06 -0500

    Arquitectura Anillo
    LO LOGRE :anguished:

--- server3000.js
+++ server3000.js
@@ -1,56 +1,14 @@
-<<<<<<< HEAD
 var express = require('express');
-var fs = require('fs');
 var axios = require('axios');
+var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
-var count = 0;
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/hola', function (req, res) {
-	res.send(msg); 
-});
-
-app.get('/', function (req, res) {
-    var html = fs.readFileSync('./pagina.html', 'utf8')
-    res.send(html);
-    var list = 1;
-    axios.post('http://127.0.0.1:3001/', list).then(response => {
-        res.render('index', { title: 'Express' });
-    }).catch(error => {
-
-        res.render('index', { title: 'Error' });
-    });
-});
-
-app.get('/test', function (req, res) {
-    axios.post('http://127.0.0.1:3001',{list:['server3001']}).then(response => {
-        res.render('index', { title: 'Express' });
-    }).catch(error => {
-        res.render('index', { title: 'Error' });
-    });
-});
-
-app.post('/', function(req, res, next) {
-    console.log(req.body);
-    res.send("ok");
-});
-
-app.listen(3000, function () {
-  console.log('Server on port 3000!');
-=======
-var express = require('express');
-var fs = require('fs');
-var axios = require('axios');
-var app = express();
-
-var count = 0;
-
+const name = "3000";
+var servNext = "3001";
+var currentLeader = name;
+var weight = 9;
+var canLeader = true;
 /*
     lider tecnico
     GOTO
@@ -59,8 +17,14 @@ var count = 0;
     micro servicis
     apache
     ingenieria orientada al caos
-    
+
  */
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -68,27 +32,90 @@ app.use(function(req, res, next) {
 });
 
 app.get('/hola', function (req, res) {
-	res.send(msg); 
+	res.send("Hola Mundo Mensaje o algo asi");
 });
 
 app.get('/', function (req, res) {
-    var html = fs.readFileSync('./pagina.html', 'utf8')
-    res.send(html);
-    var list = 1;
-    axios.post('http://127.0.0.1:3001/', list).then(response => {
-        res.render('index', { title: 'Express' });
+    res.sendFile(path.join(__dirname + '/pagina.html'));
+    /*
+    axios.post('http://localhost:3001/lounchLeader', {leader: name}).then(response => {
+        res.sendFile("OK");
     }).catch(error => {
+        res.send("Fallo");
+        console.log("error");
+    });
+    */
+});
 
-        res.render('index', { title: 'Error' });
+app.get('/leader', function (req, res) {
+    res.send("El lider Actual es: " + currentLeader);
+});
+
+app.post('/test', function (req, res) {
+    console.log("inicio" + currentLeader);
+    canLeader = false;
+    var sendLeaderData;
+    if (canLeader){
+        sendLeaderData = {leader: [name, weight]};
+    }else{
+        sendLeaderData = {leader: ["", 0]};
+    }
+    axios.post('http://localhost:' + servNext + '/newLeaderOperate', sendLeaderData).then(response => {
+        res.send("OK");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error test");
     });
 });
 
-app.get('/test', function (req, res) {
-    axios.post('http://127.0.0.1:3001',{list:['server3001']}).then(response => {
-        res.render('index', { title: 'Express' });
+app.post('/newLeaderOperate', function(req, res, next) {
+    console.log("inicio changeLeader: " + currentLeader);
+    var number = (req.body.leader[1] > weight) ? req.body.leader[1] : weight ;
+    var newLeader = (req.body.leader[1] > weight) ? req.body.leader[0] : name;
+    var sendLeaderData;
+    if (canLeader){
+        sendLeaderData = {leader: [newLeader, number]};
+    }else{
+        sendLeaderData = {leader: [req.body.leader[0], req.body.leader[1]]};
+    }
+    currentLeader = newLeader;
+    console.log("actual leader: " + sendLeaderData);
+    axios.post('http://localhost:' + servNext + '/newLeaderOperate', sendLeaderData).then(response => {
+        res.sendFile("ok");
     }).catch(error => {
-        res.render('index', { title: 'Error' });
+        res.send("Fallo");
+        console.log("error newLeader");
     });
+});
+
+app.post('/newLeaderFinish', function(req, res, next) {
+    console.log("Finish " + currentLeader);
+    axios.post('http://localhost:' + servNext + '/commentNewLeader', {name: req.body.leader[0]}).then(response => {
+        res.sendFile("ok");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error newLeader");
+    });
+});
+
+app.post('/commentNewLeader', function(req, res, next) {
+    currentLeader = req.body.name;
+    console.log("actual leader: " + currentLeader);
+    axios.post('http://localhost:' + servNext + '/commentNewLeader', {name: req.body.name}).then(response => {
+        res.sendFile("ok");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error newLeader");
+    });
+});
+
+app.post('/commentNewLeaderFinish', function(req, res, next) {
+    currentLeader = req.body.name;
+    res.send("ok");
+});
+
+app.get('/launchLeader', function(req, res, next) {
+    res.send(currentLeader);
 });
 
 app.post('/', function(req, res, next) {
@@ -96,7 +123,6 @@ app.post('/', function(req, res, next) {
     res.send("ok");
 });
 
-app.listen(3000, function () {
-  console.log('Server on port 3000!');
->>>>>>> e1241c1467f77a81b4f4a2758af209fa9a504916
+app.listen(name, function () {
+  console.log('Server on port ' + name);
 });
--- server3001.js
+++ server3001.js
@@ -1,83 +1,127 @@
-<<<<<<< HEAD
 var express = require('express');
-var fs = require('fs');
 var axios = require('axios');
+var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
-var count = 0;
+const name = "3001";
+var servNext = "3002";
+var currentLeader = name;
+var weight = 7;
+var canLeader = true;
+/*
+    lider tecnico
+    GOTO
+    FLOW QCON
+
+    micro servicis
+    apache
+    ingenieria orientada al caos
+
+ */
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.get('/hola', function (req, res) {
-	res.send(msg); 
+    res.send("Hola Mundo Mensaje o algo asi");
 });
 
 app.get('/', function (req, res) {
-	var html = fs.readFileSync('./pagina.html', 'utf8')
-	res.send(html);
+    res.sendFile(path.join(__dirname + '/pagina.html'));
+    /*
+    axios.post('http://localhost:3001/lounchLeader', {leader: name}).then(response => {
+        res.sendFile("OK");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error");
+    });
+    */
+});
+
+app.get('/leader', function (req, res) {
+    res.send("El lider Actual es: " + currentLeader);
+});
+
+app.post('/test', function (req, res) {
+    console.log("inicio" + currentLeader);
+    canLeader = false;
+    var sendLeaderData;
+    if (canLeader){
+        sendLeaderData = {leader: [name, weight]};
+    }else{
+        sendLeaderData = {leader: ["", 0]};
+    }
+    axios.post('http://localhost:' + servNext + '/newLeaderOperate', sendLeaderData).then(response => {
+        res.send("OK");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error test");
+    });
+});
+
+app.post('/newLeaderOperate', function(req, res, next) {
+    console.log("inicio changeLeader: " + currentLeader);
+    var number = (req.body.leader[1] > weight) ? req.body.leader[1] : weight ;
+    var newLeader = (req.body.leader[1] > weight) ? req.body.leader[0] : name;
+    var sendLeaderData;
+    if (canLeader){
+        sendLeaderData = {leader: [newLeader, number]};
+    }else{
+        sendLeaderData = {leader: [req.body.leader[0], req.body.leader[1]]};
+    }
+    console.log("actual leader: " + currentLeader);
+    axios.post('http://localhost:' + servNext + '/newLeaderOperate', sendLeaderData).then(response => {
+        //res.sendFile("ok");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error newLeader");
+    });
+});
+
+app.post('/newLeaderFinish', function(req, res, next) {
+    console.log("Finish " + currentLeader);
+    axios.post('http://localhost:' + servNext + '/commentNewLeader', {name: req.body.leader[0]}).then(response => {
+        res.sendFile("ok");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error newLeader");
+    });
+});
+
+app.post('/commentNewLeader', function(req, res, next) {
+    currentLeader = req.body.name;
+    console.log("actual leader: " + currentLeader);
+    axios.post('http://localhost:' + servNext + '/commentNewLeader', {name: req.body.name}).then(response => {
+        res.sendFile("ok");
+    }).catch(error => {
+        res.send("Fallo");
+        console.log("error newLeader");
+    });
+});
+
+app.post('/commentNewLeaderFinish', function(req, res, next) {
+    currentLeader = req.body.name;
+    res.send("ok");
+});
+
+app.get('/launchLeader', function(req, res, next) {
+    res.send(currentLeader);
 });
 
 app.post('/', function(req, res, next) {
-    console.log(req);
-    var list = req.body.list;
-    list.push('Server3001');
-    axios.post('http://127.0.0.1:3002/', {list: list})
-        .then(response => {
-            res.send("ok");
-        })
-        .catch(error => {
-            res.send("ok");
-        });
+    console.log(req.body);
+    res.send("ok");
 });
 
-app.listen(3001, function () {
-  console.log('Server on port 3001!');
+app.listen(name, function () {
+    console.log('Server on port ' + name);
 });
-
-
-=======
-var express = require('express');
-var fs = require('fs');
-var axios = require('axios');
-var app = express();
-
-var count = 0;
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/hola', function (req, res) {
-	res.send(msg); 
-});
-
-app.get('/', function (req, res) {
-	var html = fs.readFileSync('./pagina.html', 'utf8')
-	res.send(html);
-});
-
-app.post('/', function(req, res, next) {
-    console.log(req);
-    var list = req.body.list;
-    list.push('Server3001');
-    axios.post('http://127.0.0.1:3002/', {list: list})
-        .then(response => {
-            res.send("ok");
-        })
-        .catch(error => {
-            res.send("ok");
-        });
-});
-
-app.listen(3001, function () {
-  console.log('Server on port 3001!');
-});
-
-
->>>>>>> e1241c1467f77a81b4f4a2758af209fa9a504916
