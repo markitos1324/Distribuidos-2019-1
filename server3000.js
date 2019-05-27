cae9e6f7b959cd39b45846a8deb235d9733ab4db
Marco Antonio Gonzalez Martinez <markiros1324@gmail.com>
2019-05-27 18:37:24 -0500

    AnilloFinish
    LO ACABE!!! A CARA

---
+++ server3000.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var fs = require('fs');
+var axios = require('axios');
+var app = express();
+
+var count = 0;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/hola', function (req, res) {
+	res.send(msg); 
+});
+
+app.get('/', function (req, res) {
+    var html = fs.readFileSync('./pagina.html', 'utf8')
+    res.send(html);
+    var list = 1;
+    axios.post('http://127.0.0.1:3001/', list).then(response => {
+        res.render('index', { title: 'Express' });
+    }).catch(error => {
+
+        res.render('index', { title: 'Error' });
+    });
+});
+
+app.get('/test', function (req, res) {
+    axios.post('http://127.0.0.1:3001',{list:['server3001']}).then(response => {
+        res.render('index', { title: 'Express' });
+    }).catch(error => {
+        res.render('index', { title: 'Error' });
+    });
+});
+
+app.post('/', function(req, res, next) {
+    console.log(req.body);
+    res.send("ok");
+});
+
+app.listen(3000, function () {
+  console.log('Server on port 3000!');
+});
---
+++ server3001.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var fs = require('fs');
+var axios = require('axios');
+var app = express();
+
+var count = 0;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/hola', function (req, res) {
+	res.send(msg); 
+});
+
+app.get('/', function (req, res) {
+	var html = fs.readFileSync('./pagina.html', 'utf8')
+	res.send(html);
+});
+
+app.post('/', function(req, res, next) {
+    console.log(req);
+    var list = req.body.list;
+    list.push('Server3001');
+    axios.post('http://127.0.0.1:3002/', {list: list})
+        .then(response => {
+            res.send("ok");
+        })
+        .catch(error => {
+            res.send("ok");
+        });
+});
+
+app.listen(3001, function () {
+  console.log('Server on port 3001!');
+});
+
+
