a894b18ff7b66e97433b20c105b71532b006feec
Daniel Felipe Contreras López <cldfrkr@hotmail.com>
2019-06-02 00:48:56 -0500

    cambios simples

--- web/js/controller.js
+++ web/js/controller.js
@@ -1,45 +1,47 @@
 var app = angular.module("MyController", []);
 app.controller("ctrl", function ($scope, $http) {
-  $scope.port = "3000";
   $scope.nombre = "Servidor ";
   $scope.director = "";
   $scope.howiam ="";
   $scope.leave = false;
   $scope.servPort = "";
+  $scope.ipaddress = "10.4.74.169";
 
 
 $scope.canILeave = function() {
-  $http.get("http://10.4.74.169:"+$scope.port+"/launchLeader")
+  $http.get("http://"+$scope.ipaddress+":"+$scope.servPort+"/launchLeader")
     .then(function (response) {
-      $scope.leave=(angular.equals(response.data, $scope.port));
+      $scope.leave=(angular.equals(response.data, $scope.servPort));
     }).catch(function(e){
-      alert("Error en puerto " + $scope.port);
+      alert("Error en puerto " + $scope.servPort);
     });
  };
    $scope.consultLeader = function() {
-      $http.get("http://10.4.74.169:"+$scope.port+"/launchLeader", {timeout: 2500})
+      $http.get("http://"+$scope.ipaddress+":"+$scope.servPort+"/launchLeader", {timeout: 2500})
       .then(function (response) {
         $scope.director = "Servidor " + response.data;
         $scope.canILeave();
         $scope.howiam = ($scope.leave?"Si":"no" );
       }).catch(function(e){
-        alert("Error en puerto " + $scope.port);
+        alert("Error en puerto " + $scope.servPort);
       });
     };
 
    $scope.toLeave = function() {
     alert("Estas renunciando!!! :O");
-      $http.post("http://10.4.74.169:"+$scope.port+"/test")
+      $http.post("http://"+$scope.ipaddress+":"+$scope.servPort+"/test")
       .then(function (response) {
 
       }).catch(function(e){
-        alert("Error al renunciar " + $scope.port);
+        alert("Error al renunciar " + $scope.servPort);
       });
     };
 
     $scope.updatePort = function() {
-      console.log('Actualizando . . . ');
-        //$scope.servPort = (document.getElementById("myPortID").value);
+      console.log('Puerto actualizado ' + $scope.servPort);
+    };
+    $scope.updateIp = function() {
+      console.log('Puerto actualizado ' + $scope.ipaddress);
     };
 
 });
