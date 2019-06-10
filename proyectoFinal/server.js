78370ecf4c8aab120fb9a0ef195d8f5a62595498
Marco Antonio Gonzalez Martinez <markiros1324@gmail.com>
2019-06-10 17:25:53 -0500

    server

--- proyectoFinal/server.js
+++ proyectoFinal/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var axios = require('axios');
 var path = require('path');
+var multer = require('multer');
+var fs = require('fs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Distribuidos');
+var upload = multer({dest: 'uploads/'});
 var bodyParser = require('body-parser');
 var app = express();
 
@@ -10,10 +15,32 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization, sid");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+    next();
+});
+/*
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+*/
+
+app.post('/', upload.any(), function(req, res, next) {
+    if (req.files) {
+        req.files.forEach(function (file) {
+            console.log(file);
+
+            var filename = (new Date).valueOf() + "-" + file.originalname;
+            fs.rename(file.path, 'imagenes/' + filename, function (err) {
+                if (err) throw err;
+                console.log("file uploaded...");
+            })
+        });
+    }
 });
 
 app.get('/hola', function (req, res) {
@@ -29,6 +56,6 @@ app.post('/', function(req, res, next) {
     res.send("ok");
 });
 
-app.listen(name, function () {
-  console.log('Server on port ' + name);
+app.listen(3000, function () {
+  console.log('Server on port ' + 3000);
 });
