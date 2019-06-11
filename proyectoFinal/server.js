acb9325301c36f682fcf1fd1cfd8e1aec5936dfc
Marco Antonio Gonzalez Martinez <markiros1324@gmail.com>
2019-06-10 19:23:14 -0500

    page

--- proyectoFinal/server.js
+++ proyectoFinal/server.js
@@ -72,7 +72,7 @@ app.get('/hola', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/pagina.html'));
+    res.sendFile(path.join(__dirname + '/web/index.html'));
 });
 
 app.post('/', function(req, res, next) {
